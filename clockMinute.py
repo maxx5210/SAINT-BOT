d29efc981990253531be3c8e628fca1cfd75b5d0
FreezingKas <maxence.jung1@gmail.com>
2020-03-15 00:05:49 +0100

    new

---
+++ clockMinute.py
@@ -0,0 +1,13 @@
+from time import sleep
+from datetime import date
+import threading
+
+
+boucle = 1
+
+def boucle():
+    today = date.today()
+    print("Today's date:", today)
+    threading.Timer(60, boucle())
+
+    
